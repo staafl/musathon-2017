b4f179105876d6af7563c76295829cf3f9402ab3
Vladimir Marinov <vmarinov@tick42.com>
2017-01-29 12:24:00 +0200

    Send what notes we play to other clients

--- engine/script.js
+++ engine/script.js
@@ -53,6 +53,40 @@ var keyboard2 =
 var keyBuffer = [];
 var noteBuffer = [];
 
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}();
+
+var channelName = "/room/" + QueryString["room"];
+
+subscribe(channelName, function(event) {
+	message = JSON.parse(event.getData());
+	
+	if (message.type === "play")
+	{
+		sounds[message.instrument][message.name].play();
+	}
+});
+
 var guitarStringToY =
 {
     1: 170,
@@ -175,6 +209,7 @@ function handleKey(s, f) {
     keyBuffer.push([s, currentPosition + f, currentTime]);
     var name = stringAndFretToPitch(s, currentPosition + f, octaveShiftSamples);
     sounds["guitar"][name].play();
+	publishMessage(channelName, {type: "play", instrument: instrument, name: name});
     matchBuffers();
 }
 
